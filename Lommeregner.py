d3db8d88e4bfbafb50b1008e572ec32212e58407
tau49 <hej.du.der@hotmail.com>
2020-09-10 09:46:36 +0200

    Lommeregner v.0.2

--- Lommeregner.py
+++ Lommeregner.py
@@ -12,16 +12,17 @@ Choice = int(input("Please choose an option"))
 if Choice == 1:
     number1 = int(input("Choose the first number"))
     number2 = int(input("Choose the second number"))
-    Divider.divider(number1, number2)
+    result = Divider.divider(number1, number2)
 elif Choice == 2:
     number1 = int(input("Choose the first number"))
     number2 = int(input("Choose the second number"))
-    Gange.gange(number1, number2)
+    result = Gange.gange(number1, number2)
 elif Choice == 3:
     number1 = int(input("Choose the first number"))
     number2 = int(input("Choose the second number"))
-    Plus.plus(number1, number2)
+    result = Plus.plus(number1, number2)
 elif Choice == 4:
     number1 = int(input("Choose the first number"))
     number2 = int(input("Choose the second number"))
-    Minus.minus(number1, number2)
+    result = Minus.minus(number1, number2)
+print(result)
