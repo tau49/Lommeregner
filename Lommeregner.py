3f5a0dee2d90051c1c4fdf21b11da72ccd56ae94
tau49 <hej.du.der@hotmail.com>
2020-09-10 10:00:42 +0200

    Lommeregner v.0.3 Rework edition

--- Lommeregner.py
+++ Lommeregner.py
@@ -2,27 +2,32 @@ import Divider
 import Gange
 import Plus
 import Minus
-
-print("[1] Divider")
-print("[2] Gange")
-print("[3] Plus")
-print("[4] Minus")
-Choice = int(input("Please choose an option"))
-
-if Choice == 1:
-    number1 = int(input("Choose the first number"))
-    number2 = int(input("Choose the second number"))
-    result = Divider.divider(number1, number2)
-elif Choice == 2:
-    number1 = int(input("Choose the first number"))
-    number2 = int(input("Choose the second number"))
-    result = Gange.gange(number1, number2)
-elif Choice == 3:
-    number1 = int(input("Choose the first number"))
-    number2 = int(input("Choose the second number"))
-    result = Plus.plus(number1, number2)
-elif Choice == 4:
-    number1 = int(input("Choose the first number"))
-    number2 = int(input("Choose the second number"))
-    result = Minus.minus(number1, number2)
+while(True):
+    print("[1] Divider")
+    print("[2] Gange")
+    print("[3] Plus")
+    print("[4] Minus")
+    Choice = input("Please choose an option")
+    if Choice == "1":
+        number1 = int(input("Choose the first number"))
+        number2 = int(input("Choose the second number"))
+        result = Divider.divider(number1, number2)
+        break
+    elif Choice == "2":
+        number1 = int(input("Choose the first number"))
+        number2 = int(input("Choose the second number"))
+        result = Gange.gange(number1, number2)
+        break
+    elif Choice == "3":
+        number1 = int(input("Choose the first number"))
+        number2 = int(input("Choose the second number"))
+        result = Plus.plus(number1, number2)
+        break
+    elif Choice == "4":
+        number1 = int(input("Choose the first number"))
+        number2 = int(input("Choose the second number"))
+        result = Minus.minus(number1, number2)
+        break
+    else:
+        print("Please enter a correct number")
 print(result)
